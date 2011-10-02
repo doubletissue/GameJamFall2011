8ba8bd49bc8c3203a5dc52153d34a6aaee5e7690
Michael <michorowitz@gmail.com>
2011-10-02 14:39:05 -0400

    Updated

--- Color.py
+++ Color.py
@@ -1,7 +1,11 @@
 import	 pygame
 
 #Start at red, go right to the one before red.
-colorwheel = [pygame.Color(255, 0, 0), pygame.Color(255, 102, 0), pygame.Color(255, 148, 0), pygame.Color(254, 197, 0), pygame.Color(255, 255, 0), pygame.Color(140, 199, 0), pygame.Color(15, 173, 0), pygame.Color(0, 163, 199), pygame.Color(0, 100, 181), pygame.Color(0, 16, 165), pygame.Color(99, 0, 165), pygame.Color(197, 0, 124)]
+colorwheel = [pygame.Color(255, 0, 0), pygame.Color(255, 102, 0), pygame.Color(255, 148, 0),
+              pygame.Color(254, 197, 0), pygame.Color(255, 255, 0), pygame.Color(140, 199, 0),
+              pygame.Color(15, 173, 0), pygame.Color(0, 163, 199), pygame.Color(0, 100, 181),
+              pygame.Color(0, 16, 165), pygame.Color(99, 0, 165), pygame.Color(197, 0, 124)]
+
 red = 0
 yellow = 4
 blue = 8
