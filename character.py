ae972ba52c2cd020d7b3ee3c21ad7ff1a7f52d3c
Michael <michorowitz@gmail.com>
2011-10-02 17:47:22 -0400

    rkewr

--- character.py
+++ character.py
@@ -8,11 +8,11 @@ down = complex(0,1)
 right = complex(1,0)
 left = complex(-1,0)
 
-model_up = pygame.image.load(os.path.join("Art","player_up.png"))
-model_down = pygame.image.load(os.path.join("Art","player_down.png"))
-model_right= pygame.image.load(os.path.join("Art","player_right.png"))
-model_left = pygame.image.load(os.path.join("Art","player_left.png"))
-models = {up:model_up, down:model_down, right:model_right, left:model_left}
+#model_up = pygame.image.load(os.path.join("Art","player_up.png"))
+#model_down = pygame.image.load(os.path.join("Art","player_down.png"))
+#model_right= pygame.image.load(os.path.join("Art","player_right.png"))
+#model_left = pygame.image.load(os.path.join("Art","player_left.png"))
+#models = {up:model_up, down:model_down, right:model_right, left:model_left}
 
 #models_up = [pygame.image.load(os.path.join("Art","player_u_" + str(i) + ".png")) for i in range(12) + [-1] ]
 #models_down = [pygame.image.load(os.path.join("Art","player_d_" + str(i) + ".png")) for i in range(12) + [-1] ]
@@ -20,26 +20,34 @@ models = {up:model_up, down:model_down, right:model_right, left:model_left}
 #models_right = [pygame.image.load(os.path.join("Art","player_r_" + str(i) + ".png")) for i in range(12) + [-1] ]
 #models = {up:models_up, down:models_down, right:models_right, left:models_left}
 
+models_down = [pygame.image.load(os.path.join("Art","player","player_d_" + str(i) + ".png")) for i in range(0,12,2) + [-1] ]
+models_up = models_down
+models_right = models_down
+models_left = models_down
+
+models = {up:models_up, down:models_down, right:models_right, left:models_left}
+
 class Character(object):
   def __init__( self, world, x, y ):
     self.world = world
     self.position = complex( x, y )
     self.color = white
-    self.model = model_down
+    #self.model = model_down
+    self.model = models_down[-1]
 
   def draw( self, screen ):
     screen.blit( self.model, (self.position.real*40, self.position.imag*40) )
 
   def move( self, direction ):
-    #self.model = models[direction][self.color]
-    self.model = models[direction]
+    self.model = models[direction][self.color/2]
+    #self.model = models[direction]
     new_position = self.position + direction
     if self.world.walkable( int(new_position.real), int(new_position.imag), self.color ):
       self.position = new_position
 
   def attack( self, direction ):
-    #self.model = models[direction][self.color]
-    self.model = models[direction]
+    self.model = models[direction][self.color/2]
+    #self.model = models[direction]
     attack_position = self.position + direction
     self.color = mix_colors( self.color, self.world.hit( int(attack_position.real), int(attack_position.imag) ) )
 
