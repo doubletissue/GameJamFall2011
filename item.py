8994145e101e5cbdedc2ccb8f0195de15f93776d
jewishdan18 <jewishdan18@gmail.com>
2011-10-02 14:54:51 -0700

    stuff got erased...

--- item.py
+++ item.py
@@ -5,9 +5,9 @@ import pygame
 colors = range(12)
 
 images = [
-            pygame.image.load(os.path.join("Art","apple.png")),
-            pygame.image.load(os.path.join("Art","circlebanana.png")),
-            pygame.image.load(os.path.join("Art","giantblueberry.png"))
+            [ pygame.image.load(os.path.join("Art","Items", "1_" + str(i) + ".png")) for i in range(0,4) ],
+            [ pygame.image.load(os.path.join("Art","Items", "2_" + str(i) + ".png")) for i in range(0,4) ],
+            [ pygame.image.load(os.path.join("Art","Items", "3_" + str(i) + ".png")) for i in range(0,4) ]
          ]
 
 class Item:
@@ -15,10 +15,10 @@ class Item:
   def __init__(self, type_id):
     self.color = colors[type_id]
     self.image = images[type_id]
-    self.health = 2
+    self.health = 3
   
   def draw(self,screen,pos):
-    screen.blit(self.image,pos)
+    screen.blit(self.image[self.health],pos)
     
   def hit(self):
     if self.health == 0:
